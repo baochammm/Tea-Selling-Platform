269ffefb3fbae05eafc98e0e41871617da0d16b8
hà thục anh <hathucanh@hohihehe.lan>
2024-12-05 11:49:19 +0700

    plac order

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,73 +1,71 @@
 import { createContext, useEffect, useState } from "react";
-
-export const StoreContext = createContext(null)
+import axios from "axios";
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:3000";
+  const [token, setToken] = useState("");
+  const [tea_list, setTeaList] = useState([]);
 
-    const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:3000";
-    const [token, setToken] = useState("");
-    const [tea_list, setTeaList] = useState([]);
-
-    const addToCart = (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
-        }
-        else {
-            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
-        }
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = tea_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
+    return totalAmount;
+  };
 
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] > 0) {
-                let itemInfo = tea_list.find((product) => product._id === item);
-                totalAmount += itemInfo.price * cartItems[item];
-            }
-        }
-        return totalAmount;
+  const fetchTeaList = async () => {
+    const reponse = await axios.get(url + "/api/tea/list");
+    setTeaList(reponse.data.data);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
     }
-
-    const fetchTeaList = async () => {
-        const reponse = await axios.get(url + "/api/tea/list");
-        setTeaList(reponse.data.data);
+    async function loadData() {
+      await fetchTeaList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+      }
     }
+    loadData();
+  }, []);
 
-    useEffect(() => {
-        if (localStorage.getItem('token')) {
-            setToken(localStorage.getItem('token'))
-        }
-        async function loadData() {
-            await fetchTeaList();
-            if (localStorage.getItem('token')) {
-                setToken(localStorage.getItem('token'));
-            }
-        }
-        loadData();
-    }, [])
+  const contextValue = {
+    tea_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    url,
+    token,
+    setToken,
+  };
 
-    const contextValue = {
-        tea_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart,
-        getTotalCartAmount,
-        url,
-        token,
-        setToken
-    }
-
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export default StoreContextProvider;
